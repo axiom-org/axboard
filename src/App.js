feb40337a4aadcd3766e8e36a10293e7259a6e3f
Kevin Lacker <lacker@gmail.com>
2019-07-11 12:13:21 -0700

    scanner on local axboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+import AxiomAPI from "axiom-api";
+
 function App() {
+  let axiom = new AxiomAPI({ verbose: true });
+  let node = axiom.createNode();
+
   return (
     <div className="App">
-      <p>Hello world</p>
+      <Scanner node={node} />
+    </div>
+  );
+}
+
+function Scanner({ node }) {
+  let [lines, setLines] = useState(["scanning the network..."]);
+
+  setTimeout(() => {
+    setLines([node.statusLine()].concat(lines));
+  }, 2000);
+
+  return (
+    <div>
+      <h1>P2P Network Scanner</h1>
+      {lines.map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
     </div>
   );
 }
