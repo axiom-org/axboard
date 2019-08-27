f53256d6be3b63c85afc2c9d892d123f9046b543
Kevin Lacker <lacker@gmail.com>
2019-08-27 10:20:20 -0700

    persistence

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import AxiomAPI from "axiom-api";
-
 export default function App() {
-  let axiom = new AxiomAPI({ verbose: true });
+  let axiom = new AxiomAPI({ network: "alpha", verbose: true });
   let node = axiom.createNode();
+  let database = node.database("comments");
 
   return (
     <div className="App">
-      <Chat node={node} />
+      <Chat database={database} />
     </div>
   );
 }
@@ -18,32 +18,75 @@ class Chat extends React.Component {
   constructor(props) {
     super(props);
 
-    this.node = props.node;
+    this.database = props.database;
     this.state = {
-      clicks: 0
+      comments: {}
     };
 
-    this.node.subscribe("clicks", (sender, data) => {
-      this.setState({ clicks: this.state.clicks + 1 });
+    setInterval(() => {
+      this.database.load();
+    }, 1000);
+
+    this.database.onMessage(sm => {
+      if (sm.message.type === "Delete") {
+        return;
+      }
+      let key = sm.signer + ":" + sm.message.id;
+      let newComments = { ...this.state.comments };
+      newComments[key] = sm;
+      this.setState({ comments: newComments });
     });
+  }
+
+  sortedComments() {
+    let comments = [];
+    for (let key in this.state.comments) {
+      comments.push(this.state.comments[key]);
+    }
+    comments.sort((a, b) =>
+      a.message.timestamp.localeCompare(b.message.timestamp)
+    );
+    return comments;
   }
 
   render() {
     return (
       <div>
-        <h1>P2P Click Counting</h1>
-        <p>
-          The count is {this.state.clicks}{" "}
-          {this.state.clicks === 1 ? "click" : "clicks"}.
-        </p>
-        <button
-          onClick={() => {
-            this.node.publish("clicks", "no data");
-          }}
-        >
-          Click me
-        </button>
+        <h1>P2P Chat Proof Of Concept</h1>
+        <CommentForm database={this.database} />
+        {this.sortedComments().map((sm, index) => (
+          <p key={index}>{sm.message.data.comment}</p>
+        ))}
       </div>
     );
   }
+}
+
+function CommentForm(props) {
+  let [comment, setComment] = useState("");
+
+  let handleSubmit = e => {
+    e.preventDefault();
+    console.log(`submitting ${comment}`);
+    let data = {
+      comment: comment
+    };
+    setComment("");
+    props.database.create(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Comment:
+        <br />
+        <input
+          type="text"
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
 }
