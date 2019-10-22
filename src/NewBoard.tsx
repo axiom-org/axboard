25278e6abd9e673b7c75b856d04ff2e67ad2c04b
Kevin Lacker <lacker@gmail.com>
2019-10-22 11:12:28 -0700

    newboard form

--- src/NewBoard.tsx
+++ src/NewBoard.tsx
@@ -18,9 +18,35 @@ export default function NewBoard() {
 
   let handleSubmit = async (e: any) => {
     e.preventDefault();
+    if (name.length < 3) {
+      alert("name too short");
+      return;
+    }
     let board = await data.app.createBoard({ name, description });
     setRedirect(`/b/${board.name}/${board.id}`);
   };
 
-  return <div>TODO</div>;
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        create a new board.
+        <br />
+        name:
+        <br />
+        <input
+          value={name}
+          onChange={e => setName(e.target.value.replace(/[\W]+/g, ""))}
+        />
+      </label>
+      <label>
+        description:
+        <br />
+        <input
+          textarea={description}
+          onChange={e => setDescription(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
 }
