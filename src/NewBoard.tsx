763a27d4b359bacebe498d179cf5f542193d00ce
Kevin Lacker <lacker@gmail.com>
2019-10-22 11:04:49 -0700

    redirect to /b after creating board

--- src/NewBoard.tsx
+++ src/NewBoard.tsx
@@ -3,10 +3,24 @@ import React from "react";
 import { useDataContext } from "./DataContext";
 
 export default function NewBoard() {
+  let [name, setName] = useState("");
+  let [description, setDescription] = useState("");
+  let [redirect, setRedirect] = useState("");
+
   let data = useDataContext();
   if (!data.username) {
     return <div>You must log in to create a new board.</div>;
   }
+
+  if (redirect !== "") {
+    return <Redirect to={redirect} />;
+  }
+
+  let handleSubmit = async (e: any) => {
+    e.preventDefault();
+    let board = await data.app.createBoard({ name, description });
+    setRedirect(`/b/${board.name}/${board.id}`);
+  };
 
   return <div>TODO</div>;
 }
