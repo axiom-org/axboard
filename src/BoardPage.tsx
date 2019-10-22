87407c37db9856e87c40117e03f1f27ac9a0f030
Kevin Lacker <lacker@gmail.com>
2019-10-22 12:20:27 -0700

    BoardPage

--- src/BoardPage.tsx
+++ src/BoardPage.tsx
@@ -3,8 +3,33 @@ import React from "react";
 import { useDataContext } from "./DataContext";
 import PostList from "./PostList";
 
-export default function BoardPage(props: { id: string; name: string }) {
+export default function BoardPage(props: { id: string; name?: string }) {
   let data = useDataContext();
 
-  return <div>TODO</div>;
+  let board = data.boards[id];
+  if (!board) {
+    return <div>board not found</div>;
+  }
+  if (name && name !== board.name) {
+    return <div>the board has been renamed</div>;
+  }
+
+  let postlist = [];
+  for (let id in data.posts) {
+    let post = data.posts[id];
+    if (post.data.board === id) {
+      postlist.push(post);
+    }
+  }
+  postlist.sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime());
+
+  return (
+    <div>
+      <h2>
+        b/
+        {board.name}
+      </h2>
+      <PostList posts={postlist} />
+    </div>
+  );
 }
