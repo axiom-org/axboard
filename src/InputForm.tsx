13676d5a84f82235d14cbab1aefb07c014e8529b
Kevin Lacker <lacker@gmail.com>
2019-10-22 11:13:41 -0700

    if ta

--- src/InputForm.tsx
+++ src/InputForm.tsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 export default function InputForm(props: {
   onSubmit: (content: string) => void;
   name: string;
-  password?: boolean;
 }) {
   let [content, setContent] = useState("");
 
@@ -18,11 +17,7 @@ export default function InputForm(props: {
     <form onSubmit={handleSubmit}>
       <label>
         {props.name}:<br />
-        <input
-          type={props.password ? "password" : "text"}
-          value={content}
-          onChange={e => setContent(e.target.value)}
-        />
+        <textarea value={content} onChange={e => setContent(e.target.value)} />
       </label>
       <input type="submit" value="Submit" />
     </form>
