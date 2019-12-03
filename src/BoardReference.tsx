4adcd5c3dbc41f5260d139db8a3ac0be040b12b3
Kevin Lacker <lacker@gmail.com>
2019-12-03 14:04:46 -0800

    fix warning

--- src/BoardReference.tsx
+++ src/BoardReference.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import { AxiomObject } from "axiom-api";
 import { Link } from "react-router-dom";
 
-export default function BoardReference(props: {
-  board: AxiomObject;
-  key?: any;
-}) {
+export default function BoardReference(props: { board: AxiomObject }) {
   return (
-    <Link key={props.key} to={`/b/${props.board.name}/${props.board.id}`}>{`b/${
+    <Link to={`/b/${props.board.name}/${props.board.id}`}>{`b/${
       props.board.name
     }`}</Link>
   );
