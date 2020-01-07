280868d37ece63eab3f8d4ddd8f991908ee7a468
Kevin Lacker <lacker@gmail.com>
2020-01-07 14:31:55 -0800

    plan dataview

---
+++ src/DataView.ts
@@ -0,0 +1,10 @@
+import { AxiomObject, KeyPair } from "axiom-api";
+
+import VoteSet from "./VoteSet";
+
+type ObjectMap = { [id: string]: AxiomObject };
+type MapMap = { [parent: string]: ObjectMap };
+
+export class DataView {
+  // TODO: extract DataContextType
+}
